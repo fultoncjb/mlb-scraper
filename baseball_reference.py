3becf01d3ba6b67c3643e249db45c70b1d4c49c9
Cam Fulton <fultoncjb@gmail.com>
2022-09-28 21:35:09 -0400

    Add minimal Stathead support

--- baseball_reference.py
+++ baseball_reference.py
@@ -2,14 +2,25 @@
 baseball_reference.py
 Module used for scraping data from baseballreference.com
 """
+import datetime
+
 import bs4
 
 from beautiful_soup_helper import *
 from datetime import date, timedelta
-
+import selenium.webdriver.remote.webelement
+from selenium import webdriver
+from selenium.webdriver import remote
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import re
 
 BASE_URL = "http://www.baseball-reference.com"
-
+HITTER_RELEVANT_STAT_KEYS = ["G", "PA", "AB", "R", "H", "2B", "3B", "HR", "RBI", "SB", "CS", "BB", "SO", "TB",
+                             "GDP", "HBP", "SH", "SF", "IBB"]
 
 class PlayerIdentifier(object):
     def __init__(self, name: str, baseball_reference_id: str):
@@ -21,6 +32,13 @@ class PlayerIdentifier(object):
 
     def get_name(self) -> str:
         return self._name
+
+    def get_last_name(self) -> str:
+        return " ".join(self._name.split()[1:])
+
+
+def get_hitter_empty_stats() -> dict:
+    return {key: 0.0 for key in HITTER_RELEVANT_STAT_KEYS}
 
 
 def get_season_hitter_identifiers(year: int) -> [PlayerIdentifier]:
@@ -252,7 +270,7 @@ def get_vs_table_row_dict(soup, batter_id, pitcher_id):
     return stat_dict
 
 
-def get_all_table_row_dicts(soup: bs4.BeautifulSoup, table_name: str) -> [dict]:
+def get_all_table_row_dicts(soup: bs4.BeautifulSoup, table_name: str) -> (list, [dict]):
     """
     Get the column header labels as well as all rows in a given table in the given BeautifulSoup object
     :param soup: BeautifulSoup object containing the table_name table as a child
@@ -285,7 +303,7 @@ def get_all_table_row_dicts(soup: bs4.BeautifulSoup, table_name: str) -> [dict]:
                 stat_dict[table_header_list[i]] = stat_entries[i].text.replace(u"\xa0", " ")
         stat_dict_list.append(stat_dict)
 
-    return stat_dict_list
+    return table_header_list, stat_dict_list
 
 
 def get_table_row_dict(soup, table_name, table_row_label, table_column_label):
@@ -298,22 +316,11 @@ def get_table_row_dict(soup, table_name, table_row_label, table_column_label):
     """
 
     try:
-        table_header_list, stat_rows = get_all_table_row_dicts(soup, table_name)
+        table_header_list, stat_dicts = get_all_table_row_dicts(soup, table_name)
     except AttributeError:
         raise TableRowNotFound(table_row_label, table_column_label, table_name)
 
-    for stat_row in stat_rows:
-        # Create a dictionary of the stat attributes
-        stat_dict = dict()
-        stat_entries = stat_row.findAll(["th", "td"])
-        # The dictionary does not have valid entries, move on to the next row
-        if len(stat_entries) != len(table_header_list):
-            continue
-        for i in range(1, len(stat_entries)):
-            if stat_entries[i].text == "" or stat_entries[i].name != "td":
-                stat_dict[table_header_list[i]] = 0
-            else:
-                stat_dict[table_header_list[i]] = stat_entries[i].text.replace(u"\xa0", " ")
+    for stat_dict in stat_dicts:
         try:
             if stat_dict[table_column_label] == table_row_label:
                 return stat_dict
@@ -363,17 +370,74 @@ def get_table_body_row_dict(soup, table_name, table_row_label, table_column_labe
     raise TableRowNotFound(table_row_label, table_column_label, table_name)
 
 
-def get_career_hitting_stats(baseball_reference_id, soup=None):
+def get_career_regular_season_hitting_soup(hitter_id: str) -> BeautifulSoup:
+    url = BASE_URL + "/players/" + hitter_id[0] + "/" + str(hitter_id) + ".shtml"
+    return get_soup_from_url(url)
+
+
+def get_career_postseason_hitting_soup(hitter_id: str) -> BeautifulSoup:
+    url = BASE_URL + "/players/" + hitter_id[0] + "/" + str(hitter_id) + ".shtml"
+    return get_comment_soup_from_url(url)
+
+
+def get_hitting_stats_table(soup: BeautifulSoup, table_id: str) -> dict:
+    results_table = soup.find("table", {"id": table_id})
+    if results_table is None:
+        raise TableNotFound(table_id)
+
+    table_footer = results_table.find("tfoot")
+    if table_footer is None:
+        raise TableNotFound(table_id)
+
+    table_header = results_table.find("thead")
+    if table_header is None:
+        raise TableNotFound(table_id)
+
+    career_row_header = table_footer.find("th", {"data-stat": "player_stats_summary_explain"})
+
+    # If there is no data, then return zeros for all categories
+    if career_row_header is None:
+        return get_hitter_empty_stats()
+
+    career_row = career_row_header.parent
+    column_span = int(career_row_header["colspan"])
+    stat_labels = [x.text for x in table_header.findAll("th")[column_span:-2]]
+    stat_values = [x.text for x in career_row.findAll("td")[:-2]]
+
+    stat_dict = dict()
+    for idx in range(0, len(stat_labels)):
+        if stat_values[idx] == "":
+            stat_dict[stat_labels[idx]] = 0
+        else:
+            stat_dict[stat_labels[idx]] = stat_values[idx]
+
+    return stat_dict
+
+
+def get_career_regular_season_hitting_stats(baseball_reference_id, soup=None):
     """ Get a dictionary representation of the hitter stats for the given hitter id
     :param baseball_reference_id: unique BaseballReference ID for this hitter
     :param soup: BeautifulSoup object of the hitter career stats page (default is the URL for the given hitter)
     :return: dictionary representation of the hitter's stat home page
     """
     if soup is None:
-        url = BASE_URL + "/players/split.fcgi?id=" + str(baseball_reference_id) + "&year=Career&t=b"
-        soup = get_comment_soup_from_url(url)
+        soup = get_career_regular_season_hitting_soup(baseball_reference_id)
 
-    return get_table_row_dict(soup, "total", "Career Totals", "Split")
+    return get_hitting_stats_table(soup, "batting_standard")
+
+
+def get_career_postseason_hitting_stats(hitter_id: str, soup=None):
+    if soup is None:
+        soup = get_career_postseason_hitting_soup(hitter_id)
+
+    return get_hitting_stats_table(soup, "batting_postseason")
+
+
+def get_career_hitting_stats(baseball_reference_id: str, soup: BeautifulSoup = None) -> dict:
+    reg_season_stat_dict = get_career_regular_season_hitting_stats(baseball_reference_id)
+    playoff_stat_dict = get_career_postseason_hitting_stats(baseball_reference_id)
+
+    return {label: int(reg_season_stat_dict[label]) + int(playoff_stat_dict[label]) for label in HITTER_RELEVANT_STAT_KEYS}
 
 
 def get_vs_hand_hitting_stats(baseball_reference_id, hand_value, soup=None):
@@ -615,6 +679,257 @@ def get_team_info(team_name, year_of_interest=None, team_soup=None):
                     continue
 
     return None, None
+
+
+def login_stathead(credentials: (str, str)) -> selenium.webdriver.Firefox:
+    login_url = "https://stathead.com/users/login.cgi"
+
+    # Login to Stathead to unlock all the data
+    browser = webdriver.Firefox()
+    browser.get(login_url)
+    username_field = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.ID, "username")))
+    username_field.send_keys(credentials[0])
+    password_field = WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.ID, "password")))
+    password_field.send_keys(credentials[1])
+    login_button = WebDriverWait(browser, 20).until(
+        EC.element_to_be_clickable((By.ID, "sh-login-button")))
+    login_button.click()
+
+    return browser
+
+
+def get_vs_pitcher(hitter_id: str, min_year: int, max_year: int, credentials: (str, str)):
+
+    browser = login_stathead(credentials)
+
+    url = "https://stathead.com/baseball/batter_vs_pitcher.cgi?request=1&year_min=" + str(min_year) + "&year_max=" + str(max_year) + "&batter=" + hitter_id
+    browser.get(url)
+
+    # TODO need to now extract the data
+    # TODO return generalized object with the table data so we can pick out a specific pitcher in a helper method
+
+    # TODO we can actually get this by plate appearance using Stathead.
+    # TODO so we can save those events to the database and we can get the accurate pregame data from that
+
+    main_table = browser.find_element(By.ID, "result_table")
+    table_rows = main_table.find_elements(By.TAG_NAME, "tr")
+    for row in table_rows:
+        print(row)
+
+
+def get_vs_pitcher_gamelogs(hitter_id: PlayerIdentifier, pitcher_id: str, credentials: (str, str),
+                            browser: selenium.webdriver.Firefox = None) -> [dict]:
+    if browser is None:
+        browser = login_stathead(credentials)
+
+    url = "https://stathead.com/baseball/batter_vs_pitcher.cgi?request=1&post=1&batter=" + hitter_id.get_id() + "&pitcher=" + pitcher_id
+    browser.get(url)
+
+    main_table = browser.find_element(By.ID, "all_ajax_result_table_1")
+    table_rows = main_table.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+
+    output_rows = list()
+    team = None
+    pitcher_team = None
+    current_date = None
+    for row in table_rows:
+        if len(row.get_attribute("class")) == 0:
+
+            # Interpret what happened during the plate apperance
+            play_element = row.find_element(By.XPATH, ".//td[@data-stat='play_desc']")
+            row_dict = interpret_plate_appearance_result_string(play_element.text, hitter_id)
+            row_dict["PlayDescription"] = play_element.text
+
+            # Find the team ID
+            team_element = row.find_element(By.XPATH, ".//td[@data-stat='team_id']")
+            if team_element is not None and len(team_element.text) > 0:
+                team = team_element.text
+            if team is None:
+                raise PlayerNameNotFound # TODO need a better exception to throw
+            row_dict["HitterTeam"] = team
+
+            # Find the opposing team ID
+            pitcher_team_element = row.find_element(By.XPATH, ".//td[@data-stat='opp_ID']")
+            if pitcher_team_element is not None and len(pitcher_team_element.text) > 0:
+                pitcher_team = pitcher_team_element.text
+            if pitcher_team is None:
+                raise PlayerNameNotFound
+            row_dict["PitcherTeam"] = pitcher_team
+
+            # Find the score field
+            score_element = row.find_element(By.XPATH, ".//td[@data-stat='team_rel_score']")
+            row_dict["ScoreString"] = score_element.text
+
+            # Find the inning field
+            inning_element = row.find_element(By.XPATH, ".//td[@data-stat='inning']")
+            row_dict["Inning"] = inning_element.text
+
+            # Interpret the runners on base field
+            rob_element = row.find_element(By.XPATH, ".//td[@data-stat='runners_on_bases']")
+            is_runner_on_first = False
+            if re.search("1", rob_element.text) is not None:
+                is_runner_on_first = True
+            row_dict["RunnerOnFirst"] = is_runner_on_first
+            is_runner_on_second = False
+            if re.search("2", rob_element.text) is not None:
+                is_runner_on_second = True
+            row_dict["RunnerOnSecond"] = is_runner_on_second
+            is_runner_on_third = False
+            if re.search("3", rob_element.text) is not None:
+                is_runner_on_third = True
+            row_dict["RunnerOnThird"] = is_runner_on_third
+
+            # Find the number of outs
+            out_element = row.find_element(By.XPATH, ".//td[@data-stat='outs']")
+            row_dict["Outs"] = int(out_element.text)
+
+            # Interpret the count field (i.e. total pitches, balls, strikes
+            count_element = row.find_element(By.XPATH, ".//td[@data-stat='pitches_pbp']")
+            count_match = re.match("([0-9]*) \(([0-9]*)-([0-9]*)\)", count_element.text)
+            # Some games early on do not have these fields populated
+            if count_match is not None:
+                row_dict["Pitches"] = int(count_match.group(1))
+                row_dict["Balls"] = int(count_match.group(2))
+                row_dict["Strikes"] = int(count_match.group(3))
+
+            # Interpret the date field
+            date_element = row.find_element(By.XPATH, ".//td[@data-stat='date_game']")
+            if date_element is not None and len(date_element.text) > 0:
+                date_text = date_element.text
+                current_date = datetime.datetime.combine(datetime.date.fromisoformat(date_text), datetime.datetime.min.time())
+            if current_date is None and len(date_element.text) > 0:
+                raise PlayerNameNotFound
+            row_dict["Date"] = current_date
+
+            row_dict["Sequence"] = int(row.find_element(By.TAG_NAME, "th").text)
+            row_dict["HitterId"] = hitter_id.get_id()
+            row_dict["PitcherId"] = pitcher_id
+            output_rows.append(row_dict)
+
+    return output_rows
+
+
+def interpret_plate_appearance_result_string(input_str: str, hitter_id: PlayerIdentifier) -> dict:
+    output_dict = dict()
+
+    # Set the defaults
+    output_dict["AB"] = 0.0
+    output_dict["H"] = 0.0
+    output_dict["2B"] = 0.0
+    output_dict["3B"] = 0.0
+    output_dict["HR"] = 0.0
+    output_dict["RBI"] = 0.0
+    output_dict["BB"] = 0.0
+    output_dict["SO"] = 0.0
+    output_dict["SH"] = 0.0
+    output_dict["SF"] = 0.0
+    output_dict["IBB"] = 0.0
+    output_dict["HBP"] = 0.0
+    output_dict["GDP"] = 0.0
+    output_dict["PA"] = 1.0
+
+    is_at_bat = True
+    if is_hit_string(input_str):
+        output_dict["H"] = 1
+
+    if is_double(input_str):
+        output_dict["2B"] = 1
+    elif is_triple(input_str):
+        output_dict["3B"] = 1
+    elif re.search("Home Run", input_str) is not None:
+        output_dict["HR"] = 1
+    elif re.search("Strikeout", input_str) is not None:
+        output_dict["SO"] = 1
+    elif re.search("Intentional Walk", input_str) is not None:
+        output_dict["BB"] = 1
+        output_dict["IBB"] = 1
+        is_at_bat = False
+    elif re.search("Walk", input_str) is not None:
+        output_dict["BB"] = 1
+        is_at_bat = False
+    elif re.search("Hit By Pitch", input_str) is not None:
+        output_dict["HBP"] = 1
+        is_at_bat = False
+    elif re.search("Ground Ball Double Play", input_str) is not None:
+        output_dict["GDP"] = 1
+    elif re.search("Sacrifice Fly", input_str) is not None:
+        output_dict["SF"] = 1
+        is_at_bat = False
+    elif re.search("Bunt Groundout", input_str) is not None and re.search("Sacrifice", input_str) is not None:
+        output_dict["SH"] = 1
+        is_at_bat = False
+
+    output_dict["RBI"] = get_num_rbis(input_str, hitter_id)
+
+    if is_at_bat:
+        output_dict["AB"] = 1
+    else:
+        output_dict["AB"] = 0
+
+    return output_dict
+
+
+def is_hit_string(play_description: str) -> bool:
+    if re.search("Single", play_description) is not None or \
+       is_double(play_description) or \
+       is_triple(play_description) or \
+       re.search("Home Run", play_description) is not None:
+        return True
+
+
+def is_double(play_description: str) -> bool:
+    return re.search("Double", play_description) is not None and re.search("Double Play", play_description) is None
+
+
+def is_triple(play_description: str) -> bool:
+    return re.search("Triple", play_description) is not None and re.search("Triple Play", play_description) is None
+
+
+def get_num_rbis(play_description: str, hitter_id: PlayerIdentifier) -> int:
+    num_rbis = len(re.findall("Scores", play_description))
+    num_rbis -= len(re.findall("No RBI", play_description))
+    # If the hitter hit a home run, then they get an RBI for knocking themselves in
+    if re.search("Home Run", play_description) is not None:
+        # Sometimes for some reason Stathead includes the hitter's name as scoring and sometimes not
+        if re.search(hitter_id.get_last_name() + " Scores", play_description) is None:
+            num_rbis += 1
+    if num_rbis > 0:
+        if re.search("on E([0-9])", play_description) is not None:
+            #num_rbis -= 1
+            print("ERROR PLAY! Rbis " + str(num_rbis))
+            print(play_description)
+        if re.search("Ground Ball Double Play", play_description) is not None:
+            num_rbis = 0
+
+    return num_rbis
+
+
+def get_career_hitter_vs_ids(hitter_id: str, credentials: (str, str),
+                             browser: selenium.webdriver.Firefox = None) -> (selenium.webdriver.Firefox, [str, int]):
+    pitcher_ids = list()
+
+    if browser is None:
+        browser = login_stathead(credentials)
+
+    url = "https://stathead.com/baseball/batter_vs_pitcher.cgi?request=1&post=1&batter=" + hitter_id
+    browser.get(url)
+
+    main_table = browser.find_element(By.ID, "result_table")
+    table_rows = main_table.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+    for row in table_rows:
+        try:
+            pitcher_url = row.find_element(By.TAG_NAME, "th").get_attribute("data-append-csv")
+            plate_apperances = int(row.find_element(By.XPATH, ".//td[@data-stat='PA']").text)
+            rbis = int(row.find_element(By.XPATH, ".//td[@data-stat='RBI']").text)
+            if pitcher_url is not None:
+                pitcher_id = re.match(".*pitcher=([a-z'._]*.?[0-9]*)", pitcher_url)
+                pitcher_ids.append((pitcher_id.group(1), plate_apperances, rbis))
+        except selenium.common.exceptions.NoSuchElementException:
+            continue
+
+    return browser, pitcher_ids
 
 
 date_abbreviations = {1: "Jan",
---
+++ test/test_stathead.py
@@ -0,0 +1,57 @@
+
+from stat_miner import *
+from baseball_reference import *
+import unittest
+import sys
+
+
+class StatheadTests(unittest.TestCase):
+
+    def test_vs_pitcher(self):
+        hitter_id = HitterMiner.get_id("David Ortiz", "BOS", 2003)
+        vs_pitcher = get_vs_pitcher(hitter_id, 2007, 2009, (StatheadTests.USERNAME, StatheadTests.PASSWORD))
+
+    def test_new_vs_pitcher_abreu_pavano(self):
+        hitter_id = HitterMiner.get_id("Bobby Abreu", "PHI", 1998)
+        vs_pitcher = get_vs_pitcher_gamelogs(hitter_id, "pavanca01", (StatheadTests.USERNAME, StatheadTests.PASSWORD))
+        self.assertEqual(int(sum(item["PA"] for item in vs_pitcher)), 71)
+        self.assertEqual(int(sum(item["AB"] for item in vs_pitcher)), 59)
+        self.assertEqual(int(sum(item["H"] for item in vs_pitcher)), 20)
+        self.assertEqual(int(sum(item["2B"] for item in vs_pitcher)), 6)
+        self.assertEqual(int(sum(item["3B"] for item in vs_pitcher)), 0)
+        self.assertEqual(int(sum(item["HR"] for item in vs_pitcher)), 2)
+        self.assertEqual(int(sum(item["RBI"] for item in vs_pitcher)), 9)
+        self.assertEqual(int(sum(item["BB"] for item in vs_pitcher)), 10)
+        self.assertEqual(int(sum(item["SO"] for item in vs_pitcher)), 9)
+
+    def test_new_vs_pitcher_abreu_beckett(self):
+        hitter_id = HitterMiner.get_id("Bobby Abreu", "PHI", 1998)
+        vs_pitcher = get_vs_pitcher_gamelogs(hitter_id, "beckejo02", (StatheadTests.USERNAME, StatheadTests.PASSWORD))
+        self.assertEqual(int(sum(item["PA"] for item in vs_pitcher)), 103)
+        self.assertEqual(int(sum(item["AB"] for item in vs_pitcher)), 82)
+        self.assertEqual(int(sum(item["H"] for item in vs_pitcher)), 15)
+        self.assertEqual(int(sum(item["2B"] for item in vs_pitcher)), 3)
+        self.assertEqual(int(sum(item["3B"] for item in vs_pitcher)), 0)
+        self.assertEqual(int(sum(item["HR"] for item in vs_pitcher)), 2)
+        self.assertEqual(int(sum(item["RBI"] for item in vs_pitcher)), 7)
+        self.assertEqual(int(sum(item["BB"] for item in vs_pitcher)), 21)
+        self.assertEqual(int(sum(item["SO"] for item in vs_pitcher)), 25)
+
+    def test_get_num_rbis_errors(self):
+        hitter_id = PlayerIdentifier("David Dellucci", "delluda01")
+        num_rbis = get_num_rbis("Reached on E5 (Ground Ball); Rollins Scores/unER; Abreu Scores/No RBI/unER; Howard to 3B; Dellucci to 2B",
+                                hitter_id)
+        self.assertEqual(num_rbis, 1)
+
+    def test_get_num_rbis_errors_no_rbi(self):
+        hitter_id = PlayerIdentifier("Carlos Delgado", "delgaca01")
+        num_rbis = get_num_rbis("Reached on E9 (Fly Ball to Deep RF); Gonzalez Scores/unER/No RBI; Green Scores/unER/No RBI; Sprague Scores/unER/No RBI; Delgado to 2B",
+                                hitter_id)
+        self.assertEqual(num_rbis, 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        StatheadTests.USERNAME = sys.argv.pop()
+        StatheadTests.PASSWORD = sys.argv.pop()
+    unittest.main()
