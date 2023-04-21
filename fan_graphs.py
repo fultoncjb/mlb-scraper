9df193a544409d2865de443ef31700ddb4728ad8
Cam Fulton <fultoncjb@gmail.com>
2023-04-20 23:46:29 -0400

    Add initial FanGraphs and StatHead breakout

--- fan_graphs.py
+++ fan_graphs.py
@@ -22,7 +22,22 @@ class SeasonNotFound(Exception):
         super(SeasonNotFound, self).__init__("Season not found for year '%i' in table" % year)
 
 
-def get_hitter_id(full_name: str, team: str, year: int, browser: selenium.webdriver.Firefox = None) -> (str, str):
+"""
+FA = fastball
+SI = sinker
+SL = slider
+CH = changeup
+CU = curveball
+FC = cutter
+FS = splitter
+KN = knuckleball
+CS = slow cruve/eephus
+SB = screwball
+"""
+PITCH_TYPES = ["FA", "SI", "SL", "CH", "CU", "FC", "FS", "KN", "CS", "SB"]
+
+
+def get_hitter_id(full_name: str, team: str, year: int) -> (str, str):
     """
     Get the unique identifying information for a particular hitter who played for a particular team in a particular season.
     Note: the unique identifying information for a player involves their name, a unique integer, and their position.
@@ -47,23 +62,25 @@ def get_hitter_id(full_name: str, team: str, year: int, browser: selenium.webdri
         regex_match = re.match("([a-zA-Z.']* [a-zA-Z.']*)", row.text)
         if regex_match is not None and regex_match.group(1) == full_name:
             hitter_link = row.find("a").get("href")
-            id_position_regex_match = re.search("playerid=([0-9]*)&position=([A-Z]*)", hitter_link)
+            id_position_regex_match = re.search("playerid=([0-9]*)&position=([0-9A-Z]*)", hitter_link)
             if id_position_regex_match is not None:
                 return full_name.lower().replace(" ", "-") + "/" + id_position_regex_match.group(1), id_position_regex_match.group(2)
 
     raise PlayerNameNotFound(full_name)
 
 
-def get_hitter_stats_vs_pitch(fan_graphs_id_tuple: (str, str), year: int, pitch_type: str):
+def get_hitter_stats_vs_pitch(fan_graphs_id_tuple: (str, str), year: int, pitch_type: str, browser: selenium.webdriver.Firefox = None) -> dict:
     url = BASE_URL + "/players/" + fan_graphs_id_tuple[0] + "/pitch-type-splits?position=" + fan_graphs_id_tuple[1] + "&data=pi&pitchtype=" + pitch_type
 
-    browser = webdriver.Firefox()
+    if browser is None:
+        browser = webdriver.Firefox()
+
     browser.get(url)
 
     # Close promotional modal if it exists
     button_class_name = "CloseButton__ButtonElement-sc-79mh24-0 eXoQFD loneoak-CloseButton loneoak-close loneoak-ClosePosition--top-right"
     try:
-        modal_button = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, button_class_name)))
+        modal_button = WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, button_class_name)))
         modal_button.click()
     except selenium.common.exceptions.TimeoutException:
         print("Warning! FanGraph promotional modal not present")
@@ -77,8 +94,11 @@ def get_hitter_stats_vs_pitch(fan_graphs_id_tuple: (str, str), year: int, pitch_
         is_correct_year = False
         for field in row.find_elements(By.TAG_NAME, "td"):
             field_name = field.get_attribute("data-stat")
-            if field_name == "Season" and int(field.text) == year:
-                is_correct_year = True
+            try:
+                if field_name == "Season" and int(field.text) == year:
+                    is_correct_year = True
+                    break
+            except ValueError:
                 break
 
         if is_correct_year:
